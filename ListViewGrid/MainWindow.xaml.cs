f53ab620cc8eba759fe7c8315f87afe2e5db936a
Sebastiano Del Gobbo <sebdg@tiscali.it>
2024-11-25 17:16:50 +0100

    ADD: ListViewGrid: Progetto per testare l'utilizzo di un LsitView con una vista di tipo Grid, utilizzando il tema Fluent per WPF

---
+++ ListViewGrid/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace ListViewGrid;
+
+public partial class MainWindow : Window
+{
+	public List<Person> Persons { get; set; }
+
+	public MainWindow()
+	{
+		this.InitializeComponent();
+
+		this.Persons = new List<Person>()
+		{
+			new Person("John", "Doe"),
+			new Person("Frank", "Zappa"),
+			new Person("David", "Gilmour"),
+			new Person("Mark", "Knopfler"),
+			new Person("John", "Lee Hooker"),
+		};
+
+		this.DataContext = this;
+	}
+}
---
+++ ListViewGrid/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewGrid
+{
+	public class Person
+	{
+		public String FirstName { get; set; }
+		public String LastName { get; set; }
+
+		public Person()
+		{
+			this.FirstName = String.Empty;
+			this.LastName = String.Empty;
+		}
+
+		public Person(String firstName, String lastName)
+		{
+			this.FirstName = firstName;
+			this.LastName = lastName;
+		}
+	}
+}
