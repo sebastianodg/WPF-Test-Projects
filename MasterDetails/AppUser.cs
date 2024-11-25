36b749d41a637116612e9d5b70cc6a53eab72ea4
Sebastiano Del Gobbo <sebdg@tiscali.it>
2024-11-25 20:45:42 +0100

    ADD: Progetto MasterDetails con l'utilizzo della libreria WPF UI - Non ultimato

---
+++ MasterDetails/AppUser.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MasterDetails;
+
+public partial class AppUser : ObservableObject
+{
+	[ObservableProperty]
+	private String _userName;
+
+	[ObservableProperty]
+	private String _password;
+
+	[ObservableProperty]
+	private String _firstName;
+
+	[ObservableProperty]
+	private String _lastName;
+
+	[ObservableProperty]
+	private String _email;
+
+	[ObservableProperty]
+	private Boolean _IsAdministrator;
+
+	public AppUser()
+	{
+		this._userName = String.Empty;
+		this._password = String.Empty;
+		this._firstName = String.Empty;
+		this._lastName = String.Empty;
+		this._email = String.Empty;
+		this._IsAdministrator = false;
+	}
+
+	public AppUser(String userName, String password, String firstName, String lastName, String email, Boolean isAdministrator)
+	{
+		this._userName = userName;
+		this._password = password;
+		this._firstName = firstName;
+		this._lastName = lastName;
+		this._email = email;
+		this._IsAdministrator = isAdministrator;
+	}
+}
---
+++ MasterDetails/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Wpf.Ui;
+using Wpf.Ui.Controls;
+
+namespace MasterDetails;
+
+public partial class MainWindow : INavigationWindow
+{
+	private MainWindowVM _vm;
+
+	public MainWindow()
+	{
+		this.InitializeComponent();
+
+		this._vm = new MainWindowVM();
+		this.DataContext = this._vm;
+	}
+
+	public INavigationView GetNavigation()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Boolean Navigate(Type pageType)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void SetServiceProvider(IServiceProvider serviceProvider)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void SetPageService(IPageService pageService)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void ShowWindow()
+	{
+		throw new NotImplementedException();
+	}
+
+	public void CloseWindow()
+	{
+		throw new NotImplementedException();
+	}
+}
