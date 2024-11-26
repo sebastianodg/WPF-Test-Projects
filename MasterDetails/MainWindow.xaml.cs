42f2a10cae1e5d4605393824497d301071899f78
Sebastiano Del Gobbo <sebdg@tiscali.it>
2024-11-26 18:04:02 +0100

    ADD: MasterDetails: Finestra di dialogo per la conferma dell'eliminazione dell'utente

--- MasterDetails/MainWindow.xaml.cs
+++ MasterDetails/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
-﻿using Wpf.Ui;
+﻿using System.Windows;
+using Wpf.Ui;
+using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
 
 namespace MasterDetails;
 
-public partial class MainWindow : INavigationWindow
+public partial class MainWindow : FluentWindow
 {
 	private MainWindowVM _vm;
 
@@ -13,6 +15,20 @@ public partial class MainWindow : INavigationWindow
 
 		this._vm = new MainWindowVM();
 		this.DataContext = this._vm;
+
+		this.Loaded += this.OnWindowLoaded;
+		this.Unloaded += this.OnWindowUnloaded;
+
+	}
+
+	private void OnWindowLoaded(Object sender, RoutedEventArgs e)
+	{
+		SystemThemeWatcher.Watch(this, WindowBackdropType.Mica, true);
+	}
+
+	private void OnWindowUnloaded(Object sender, RoutedEventArgs e)
+	{
+		SystemThemeWatcher.UnWatch(this);
 	}
 
 	public INavigationView GetNavigation()
@@ -43,5 +59,18 @@ public partial class MainWindow : INavigationWindow
 	public void CloseWindow()
 	{
 		throw new NotImplementedException();
+	}
+
+	private async void Prova(Object sender, System.Windows.RoutedEventArgs e)
+	{
+		Wpf.Ui.Controls.MessageBox messageBox = new Wpf.Ui.Controls.MessageBox()
+		{
+			Title = "Confirm",
+			Content = "This will permanently delete the user. Are you sure?",
+			CloseButtonText = "No",
+			SecondaryButtonText = "Yes",
+			SecondaryButtonAppearance = ControlAppearance.Danger,
+		};
+		await messageBox.ShowDialogAsync();
 	}
 }
